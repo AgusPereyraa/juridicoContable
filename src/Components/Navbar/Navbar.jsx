20a6858f4ad1dda1b46b5b05e44417f4edb3508f
Agustin <pereyra.agus@outlook.com.ar>
2023-05-12 19:43:00 -0300

    navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,8 +1,152 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './navbar.css'
+import { Link as Anchor, } from "react-router-dom";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser, faHeart, faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 
-export default function Navbar() {
+function NavScrollExample() {
+
+  let [scrolled, setScrolled] = useState(false);
+  let [modal, setModal] = useState(false);  //Se define Modal para 'favoritos'
+  let [modalCart, setModalCart] = useState(false); //Se define Modal para 'carrito'
+  let [modalUser, setModalUser] = useState(false); //Se define Modal para 'user'
+  let [isOpen, setIsOpen] = useState(false)  //Se define Modal para 'Navbar'
+  let [modalUserOption, setModalUserOption] = useState('login');
+
+  const handleModal = () => {
+    setModal(!modal);
+}; //Funcion renderiza Modal 'favoritos'
+
+const handleModalCart = () => {
+    setModalCart(!modalCart);
+
+}; //Funcion renderiza Modal 'Carrito'
+
+const handleModalUser = () => {
+    setModalUser(!modalUser);
+}; //Funcion renderiza Modal 'user'
+
+const handleModalUserOption = () => {
+    setModalUserOption(modalUserOption === 'register' ? 'login' : 'register');
+}; //Funcion renderiza el modal 'register' o 'login'
+
+
+// Cambio de estado en el Navbar cuando se scrollea 
+useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+        window.removeEventListener("scroll", handleScroll);
+    };
+}, []);
+
+// Cambio de color en navbar al hacer scroll
+const handleScroll = () => {
+    const offset = window.scrollY;
+    if (offset > 0) {
+        setScrolled(true);
+    } else {
+        setScrolled(false);
+    }
+};
+// --------------------------------------------- //
+
+
   return (
-    <div>Navbar</div>
-  )
+    <header>
+
+            <nav className={scrolled ? "navbar scrolled " : "navbar"}>
+                {/* <div className='logo'>
+                    <img src={Logo} alt="logo" />
+                </div>
+                <div className='logo2'>
+                    <img src={Logo2} alt="logo" />
+                </div> */}
+
+                <div className={`nav_items  ${isOpen && "open"}`} >
+                    {/* <div className="cerrar-nav" onClick={() => setIsOpen(!isOpen)}>
+                        x
+                    </div>
+                    <div className='logo-nav'>
+                        <img src={Logo} alt="logo" />
+                    </div>
+                    <div className='logo2-nav'>
+                        <img src={Logo2} alt="logo" />
+                    </div> */}
+
+                    <div className='iconos2'>
+
+                        <FontAwesomeIcon icon={faShoppingCart} onClick={handleModalCart} />
+                        <FontAwesomeIcon icon={faUser} onClick={handleModalUser} />
+                    </div>
+
+                    <div>
+
+                        <div className='enlaces'>
+                            <Anchor to={`/`} >Home</Anchor>
+                            <Anchor to={`/destinos`} >Destinations</Anchor>
+                            <Anchor to={`/faqs`} >FAQ's</Anchor>
+                            <Anchor to={`/blog`} >Blog</Anchor>
+                        </div>
+
+                        <div className="redes-sociales">
+                            <Anchor to={`/`}><i className='fa fa-facebook'></i></Anchor>
+                            <Anchor to={`/`}><i className='fa fa-instagram'></i></Anchor>
+                            <Anchor to={`/`}> <i className='fa fa-linkedin'></i></Anchor>
+                            <Anchor to={`/`}> <i className='fa fa-whatsapp'></i></Anchor>
+                        </div>
+                    </div>
+
+                </div>
+
+
+                <div className='icons-nav'>
+                    {/* <FontAwesomeIcon icon={faHeart} onClick={handleModal} /> */}
+                    <FontAwesomeIcon icon={faShoppingCart} onClick={handleModalCart} />
+                    <FontAwesomeIcon icon={faUser} onClick={handleModalUser} />
+                </div>
+
+                <div className={`nav_toggle  ${isOpen && "open"}`} onClick={() => setIsOpen(!isOpen)}>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+
+                {/* {modal && (
+                    <div className="modal_content ">
+                        <div className="modal-nav">
+                            <div className="cerrar-modal" onClick={handleModal}>x</div>
+                            <h3>Favourite</h3>
+                            <Favorito />
+                        </div>
+                    </div>
+                )} */}
+                {modalCart && (
+                    <div className="modal_content">
+                        <div className="modal-nav">
+                            <div className="cerrar-modal" onClick={handleModalCart}>x</div>
+                            <h3>Your Cart</h3>
+
+                            {/* <Carrito /> */}
+                        </div>
+                    </div>
+                )}
+
+                {modalUser && (
+                    <div className="modal_content">
+                        <div className="modal-nav">
+                            <div className="cerrar-modal" onClick={handleModalUser}>x</div>
+                            <h3>Your Profile</h3>
+                            {/* <UserInfo /> */}
+                            {/* {modalUserOption === 'register' ? <Register /> : <LogIn />} */}
+                            <p className='loginText'>Already have an account? <Anchor id='login' onClick={handleModalUserOption}>{modalUserOption === 'register' ? 'Log In' : 'Register'}</Anchor></p>
+                        </div>
+                    </div>
+                )}
+
+            </nav>
+
+        </header >
+  );
 }
+
+export default NavScrollExample;
