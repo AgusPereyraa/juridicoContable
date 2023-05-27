d20ec0d2b635776f48492aabdcfb762e2d800986
Agustin <pereyra.agus@outlook.com.ar>
2023-05-27 01:12:47 -0300

    router dom estructura

--- src/Components/MainHome/MainHome.jsx
+++ src/Components/MainHome/MainHome.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import './mainHome.css'
+import H2 from '../H2/H2'
+import P from '../P/P'
+import Span from '../Span/Span'
+// import { Link as Anchor } from 'react-router-dom'
 
 export default function MainHome() {
     return (
         <div className='home'>
-                <h2 className='homeTitle'>¿Necesitas ayuda legal o contable?</h2>
-                <p className='homeTitle'>¡Podemos ayudarte!</p>
-                <p className='homeDescription'>En el Estudio Jurídico Contable V & P, ofrecemos una amplia gama de servicios legales y contables para ayudarte en todo lo que necesites. Desde asesoría fiscal y contable hasta litigios civiles y comerciales. Contáctanos hoy para obtener más información sobre cómo podemos ayudarte a resolver tus problemas legales y contables.</p>
-                <button className='homeButton'>Contactanos</button>
+            <H2 text='¿Necesitas ayuda legal o contable?'/>
+            <P text='¡Podemos ayudarte!'/>
+            <Span id='descHome' text='En el Estudio Jurídico Contable V & P, ofrecemos una amplia gama de servicios legales y contables para ayudarte en todo lo que necesites. Desde asesoría fiscal y contable hasta litigios civiles y comerciales. Contáctanos hoy para obtener más información sobre cómo podemos ayudarte a resolver tus problemas legales y contables.'/>
+            <button className='homeButton'>Contactanos</button> {/* Anchor as Link*/}
         </div>
     )
 }
