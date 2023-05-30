4ce8fb5a95141aa4cf3cdf0cf423d5b8238e30b1
Agustin <pereyra.agus@outlook.com.ar>
2023-05-30 19:01:44 -0300

    fix app

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import './App.css';
-// import IndexLayout from './Layout/IndexLayout/IndexLayout';
-// import Index from './Components/Index/Index';
 import {router} from './Pages/index.js'
 import { RouterProvider } from 'react-router-dom';
 
@@ -8,9 +6,6 @@ function App() {
   return (
     <div className="container">
       <RouterProvider router={router}/>
-      {/* <IndexLayout>
-        <Index />
-      </IndexLayout> */}
     </div>
   );
 }
