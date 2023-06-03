b3de84e7aa107f28814f24dadd34e55b08a43e3d
Agustin <pereyra.agus@outlook.com.ar>
2023-06-02 23:36:06 -0300

    about

--- src/Components/HomeAbout/HomeAbout.jsx
+++ src/Components/HomeAbout/HomeAbout.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import './homeAbout.css'
+import H2 from '../H2/H2'
 
 export default function HomeAbout() {
   return (
     <div className='homeAbout'>
-
+      <H2 />
     </div>
   )
 }
