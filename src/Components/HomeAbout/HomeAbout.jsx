5cec6e732207ebf5ffe519137a9ceb75fd43f214
Agustin <pereyra.agus@outlook.com.ar>
2023-06-03 23:03:23 -0300

    import components

--- src/Components/HomeAbout/HomeAbout.jsx
+++ src/Components/HomeAbout/HomeAbout.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import './homeAbout.css'
 import H2 from '../H2/H2'
+import P from '../P/P'
 
 export default function HomeAbout() {
   return (
     <div className='homeAbout'>
       <H2 />
+      <P />
     </div>
   )
 }
