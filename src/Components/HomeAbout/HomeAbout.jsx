d7c76bd2d47b206f53882446800ffd7aa0b222ba
Agustin <pereyra.agus@outlook.com.ar>
2023-06-07 20:03:23 -0300

    banner about contact

--- src/Components/HomeAbout/HomeAbout.jsx
+++ src/Components/HomeAbout/HomeAbout.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import './homeAbout.css'
 import H2 from '../H2/H2'
+import H3 from '../H3/H3'
 import H4 from '../H4/H4'
 import P from '../P/P'
+import Span from '../Span/Span'
 import Admin from '../../Img/profile-circle.svg'
+import Logo1 from '../../Img/Logo1.png'
+import { Link as Anchor } from 'react-router-dom'
 
 export default function HomeAbout() {
   return (
@@ -45,6 +49,29 @@ export default function HomeAbout() {
             <img src={Admin} alt="" />
             <H4 className='adminTitle' text='Nadia Pereyra' />
             <P className='adminDesc' text='Abogada, recibida de la UBA, con especialización y experiencia en el area de Derecho Empresarial.' />
+          </div>
+
+        </div>
+
+        <div className='aboutContact'>
+
+          <div className='aboutTitle'>
+            <H3 text='Contactanos' />
+          </div>
+
+          <div className='about'>
+
+            <div className='aboutLeft'>
+              <P text='Si buscas una consultoría jurídica que pueda brindarte soluciones legales y fiscales efectivas y personalizadas, no dudes en contactarnos. Nuestro equipo de abogados y contadores está listo para ayudarte a cumplir con tus objetivos empresariales y mantener tu negocio en el camino del éxito.' />
+              <Anchor to='/contact'>
+                  <Span text='Contactanos'/>
+              </Anchor>
+            </div>
+
+            <div className='aboutRight'>
+              <img src={Logo1} alt="logoContact" />
+            </div>
+
           </div>
 
         </div>
