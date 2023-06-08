74efcebb4b6562d479d8a94ad7fc205da9a50676
Agustin <pereyra.agus@outlook.com.ar>
2023-06-08 03:18:03 -0300

    components contact

---
+++ src/Components/HomeContact/HomeContact.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './homeContact.css'
+import H2 from '../H2/H2'
+import P from '../P/P'
+
+export default function HomeContact() {
+  return (
+    <div className='homeContact'>
+
+        <div className='textContact'>
+            <H2 text='Contáctanos' />
+            <P text='No dudes en contactarnos. Somos el asesor que necesita tu PYME, creemos que juntos es mejor y que el trabajo en comunidad da sus frutos. La primera consulta es sin cargo. ' />
+        </div>
+        
+    </div>
+  )
+}
