a0046dbff3deae1920f8a26bae5ec248877049e3
Agustin <pereyra.agus@outlook.com.ar>
2023-06-09 12:20:50 -0300

    emailjs installed

--- src/Components/HomeContact/HomeContact.jsx
+++ src/Components/HomeContact/HomeContact.jsx
@@ -1,37 +1,53 @@
-import React from 'react'
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 import './homeContact.css'
 import H2 from '../H2/H2'
 import P from '../P/P'
 import Logo2 from '../../Img/Logo2.png'
 
 export default function HomeContact() {
+
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_tmggftq', 'template_pccebwj', form.current, '4pRat-Hd2RpTin-Nr')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+      e.target.reset()
+  };
+
   return (
     <div className='homeContact'>
 
-        <div className='textContact'>
-            <H2 text='Contáctanos' />
-            <P text='No dudes en contactarnos. Somos el asesor que necesita tu PYME, creemos que juntos es mejor y que el trabajo en comunidad da sus frutos. La primera consulta es sin cargo. ' />
+      <div className='textContact'>
+        <H2 text='Contáctanos' />
+        <P text='No dudes en contactarnos. Somos el asesor que necesita tu PYME, creemos que juntos es mejor y que el trabajo en comunidad da sus frutos. La primera consulta es sin cargo. ' />
+      </div>
+
+      <div className='formContact'>
+
+        <div className='allenar'>
+          <img src={Logo2} alt="" />
         </div>
 
-        <div className='formContact'>
+        <div className='form'>
 
-          <div className='allenar'>
-            <img src={Logo2} alt="" />
-          </div>
+          <form ref={form} onSubmit={sendEmail} className='input'>
+            <input type="text" name={"user_name"} className='inputForm' placeholder='Nombre*'/>
+            <input type="email" name={"user_email"} className='inputForm'placeholder='Email*'/>
+            <textarea name={"message"} className='inputForm' placeholder='Deje su mensaje.'/>
+            <input type="submit" value="Enviar" />
+          </form>
 
-          <div className='form'>
-
-            <form action='post' className='input'>
-              <input type="text" className='inputForm' placeholder='Nombre*'/>
-              <input type="text" className='inputForm' placeholder='Email*'/>
-              <input type="text-area" className='inputForm' placeholder='Deje su mensaje.'/>
-            </form>
-              <button type="submit">Enviar</button>
-
-          </div>
-          
         </div>
-        
+
+      </div>
+
     </div>
   )
 }
