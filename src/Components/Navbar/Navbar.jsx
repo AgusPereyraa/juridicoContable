473a48aa431f82012638f73739c0f4f023fda006
Agustin <pereyra.agus@outlook.com.ar>
2023-06-27 14:53:17 -0300

    fixes responsive home

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,50 +1,70 @@
-import React, {useState} from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link as Anchor } from 'react-router-dom'
 import P from '../P/P'
 import Span from '../Span/Span'
 import './navbar.css'
 
 
-const Navbar = ()=>{
+const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false)
+  const [scrolled, setScrolled] = useState(false);
 
-return (
- 
-  <nav>
-   <div className='navbar'>
-    
-    <div className={`nav_items ${isOpen && "open"}`}>
-      <div className='porfile'>
-    
-        <div className='porfile-text'>
+
+  // Cambio de estado en el Navbar cuando se scrollea 
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  // Cambio de color en navbar al hacer scroll
+  const handleScroll = () => {
+    const offset = window.scrollY;
+    if (offset > 0) {
+      setScrolled(true);
+    } else {
+      setScrolled(false);
+    }
+  };
+
+  return (
+
+    <nav>
+      <div className={scrolled ? "navbar scrolled" : "navbar"}>
+
+        <div className={`nav_items ${isOpen && "open"}`}>
+          <div className='porfile'>
+
+            <div className='porfile-text'>
+              <P text='Estudio Juridico Contable' />
+              <P text='V & P' />
+            </div>
+
+          </div>
+
+          <Anchor to='/'><P text='Inicio' /></Anchor>
+          <Anchor to='/services'><P text='Servicios' /></Anchor>
+          <Anchor to='/about'><P text='Nosotros' /></Anchor>
+          <Anchor to='/prices'><P text='Honorarios' /></Anchor>
+          <Anchor to='/contact'><P text='Contacto' /></Anchor>
+        </div>
+
+        <div className={`nav_toggle  ${isOpen && "open"}`} onClick={() => setIsOpen(!isOpen)}>
+          <Span />
+          <Span />
+          <Span />
+        </div>
+
+        <div className='title-nav'>
           <P text='Estudio Juridico Contable' />
           <P text='V & P' />
         </div>
 
       </div>
 
-        <Anchor to='/'><P text='Inicio' /></Anchor>
-        <Anchor to='/services'><P text='Servicios' /></Anchor>
-        <Anchor to='/about'><P text='Nosotros' /></Anchor>
-        <Anchor to='/prices'><P text='Honorarios' /></Anchor>
-        <Anchor to='/contact'><P text='Contacto' /></Anchor>
-    </div>
-
-    <div className={`nav_toggle  ${isOpen && "open"}`} onClick={()=> setIsOpen(!isOpen)}>
-      <Span />
-      <Span />
-      <Span />
-    </div>
-    
-    <div className='title-nav'>
-      <P text='Estudio Juridico Contable' />
-      <P text='V & P' />
-    </div>
-  
-  </div>
-
- </nav>
-)
+    </nav>
+  )
 
 }
 
