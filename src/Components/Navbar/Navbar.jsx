f94b98a5c2760ab1e10152071fd34bee3862406b
Agustin <pereyra.agus@outlook.com.ar>
2023-07-09 23:40:29 -0300

    navbar fix

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -57,16 +57,18 @@ const Navbar = () => {
           <Span />
         </div>
 
-        <div className='whatsapp-nav'>
-          <a href="https://api.whatsapp.com/send?phone=+541122523564&text=Hola%20%C2%BFC%C3%B3mo%20est%C3%A1s?%20Estuve%20mirando%20tu%20Portfolio"
-            target="_blank" rel='noreferrer'>
-            <img src={WhatsApp} alt="WhatsApp" />
-          </a>
-        </div>
+        <div className='navbar-right'>
+          <div className='whatsapp-nav'>
+            <a href="https://api.whatsapp.com/send?phone=+541122523564&text=Hola%20%C2%BFC%C3%B3mo%20est%C3%A1s?%20Estuve%20mirando%20tu%20Portfolio"
+              target="_blank" rel='noreferrer'>
+              <img src={WhatsApp} alt="WhatsApp" />
+            </a>
+          </div>
 
-        <div className='title-nav'>
-          <P text='Estudio Juridico Contable' />
-          <P text='V & P' />
+          <div className='title-nav'>
+            <P text='Estudio Juridico Contable' />
+            <P text='V & P' />
+          </div>
         </div>
 
       </div>
